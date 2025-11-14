c367bb2ec6f171bc8932dfc3c100610a7f14e768
Zoe Carver <zcarver@tenstorrent.com>
2025-11-14 00:01:54 -0500

    Update test_chained_matmul to use separate kernels

--- test/python/test_chained_matmul.py
+++ test/python/test_chained_matmul.py
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+# Tests chained matrix multiplications using separate kernels with intermediate CB
+#   note to zoe: 7b59bec15a90c92b70d15d11e6a4784b41832757 has the change with MemRefProvenanceAnalysis to fix this.
 # UNSUPPORTED: system-darwin
 # RUN: %python %s > %t.output.txt 2>&1
 # RUN: FileCheck %s < %t.initial.mlir
@@ -11,50 +13,79 @@ import torch
 from ttlang.d2m_api import *
 
 
-@pykernel_gen(grid=(1, 1), block_factors=[(1, 1), (1, 1), (1, 1), (1, 1)])
-def test_chained_matmul(a, b, c, out):
+# First matmul: A @ B
+@pykernel_gen(grid=(1, 1), block_factors=[(1, 1), (1, 1), (1, 1)])
+def matmul_ab(a, b, temp_out):
     a_accessor = TensorAccessor(a)
     b_accessor = TensorAccessor(b)
-    c_accessor = TensorAccessor(c)
 
     @compute()
-    async def matmul_compute(a_cb: CircularBuffer, b_cb: CircularBuffer, c_cb: CircularBuffer, out_cb: CircularBuffer):
+    async def compute_ab(a_cb: CircularBuffer, b_cb: CircularBuffer, out_cb: CircularBuffer):
         a_val = a_cb.wait()
         b_val = b_cb.wait()
-        c_val = c_cb.wait()
         o = out_cb.reserve()
-        # (A @ B) @ C
-        temp = a_val @ b_val
-        result = temp @ c_val
+        result = a_val @ b_val
         o.store(result)
         a_cb.pop()
         b_cb.pop()
-        c_cb.pop()
         out_cb.push()
 
     @datamovement()
-    async def dm_a(a_cb: CircularBuffer, b_cb: CircularBuffer, c_cb: CircularBuffer, out_cb: CircularBuffer):
+    async def dm_a(a_cb: CircularBuffer, b_cb: CircularBuffer, out_cb: CircularBuffer):
         a_shard = a_cb.reserve()
         tx = dma(a_accessor[0, 0], a_shard)
         tx.wait()
 
     @datamovement()
-    async def dm_b(a_cb: CircularBuffer, b_cb: CircularBuffer, c_cb: CircularBuffer, out_cb: CircularBuffer):
+    async def dm_b(a_cb: CircularBuffer, b_cb: CircularBuffer, out_cb: CircularBuffer):
         b_shard = b_cb.reserve()
         tx = dma(b_accessor[0, 0], b_shard)
         tx.wait()
 
+    return Program(compute_ab, dm_a, dm_b)(a, b, temp_out)
+
+
+# Second matmul: temp @ C
+@pykernel_gen(grid=(1, 1), block_factors=[(1, 1), (1, 1), (1, 1)])
+def matmul_temp_c(temp_in, c, out):
+    temp_accessor = TensorAccessor(temp_in)
+    c_accessor = TensorAccessor(c)
+
+    @compute()
+    async def compute_temp_c(temp_cb: CircularBuffer, c_cb: CircularBuffer, out_cb: CircularBuffer):
+        temp_val = temp_cb.wait()
+        c_val = c_cb.wait()
+        o = out_cb.reserve()
+        result = temp_val @ c_val
+        o.store(result)
+        temp_cb.pop()
+        c_cb.pop()
+        out_cb.push()
+
     @datamovement()
-    async def dm_c(a_cb: CircularBuffer, b_cb: CircularBuffer, c_cb: CircularBuffer, out_cb: CircularBuffer):
+    async def dm_temp(temp_cb: CircularBuffer, c_cb: CircularBuffer, out_cb: CircularBuffer):
+        temp_shard = temp_cb.reserve()
+        tx = dma(temp_accessor[0, 0], temp_shard)
+        tx.wait()
+
+    @datamovement()
+    async def dm_c(temp_cb: CircularBuffer, c_cb: CircularBuffer, out_cb: CircularBuffer):
         c_shard = c_cb.reserve()
         tx = dma(c_accessor[0, 0], c_shard)
         tx.wait()
 
-    return Program(matmul_compute, dm_a, dm_b, dm_c)(a, b, c, out)
+    return Program(compute_temp_c, dm_temp, dm_c)(temp_in, c, out)
 
 
-# CHECK: func.func @test_chained_matmul
+def test_chained_matmul(a, b, c, out):
+    temp = torch.zeros(32, 32)
+    matmul_ab(a, b, temp)
+    matmul_temp_c(temp, c, out)
+
+
+# CHECK: func.func @matmul_ab
 # CHECK: "d2m.tile_matmul"
+# CHECK: func.func @matmul_temp_c
 # CHECK: "d2m.tile_matmul"
 
 a = torch.eye(32)
