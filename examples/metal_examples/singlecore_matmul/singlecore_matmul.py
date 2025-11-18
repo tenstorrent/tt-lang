7d283e84cec2956a83ff806650a41fafe8b85368
alex richins <arichins@tenstorrent.com>
2025-11-18 01:06:41 +0000

    single core

--- examples/metal_examples/singlecore_matmul/singlecore_matmul.py
+++ examples/metal_examples/singlecore_matmul/singlecore_matmul.py
@@ -1,7 +1,33 @@
 import ttnn
 import pytest
+import torch
 
 
+# need to figure out what device is
+def test_singlecore_matmul(device): 
+    M, K, N = 640, 640, 640
 
-def test_singlecore_matmul():
     
+    num_tiles = 4
+    src_bank_id = 0
+    dst_bank_id = 0
+
+    shape = [1, num_tiles, 32, 32]
+    data = torch.rand(shape).to(torch.bfloat16)
+
+    dram_memory_config = ttnn.DRAM_MEMORY_CONFIG
+
+    a_tensor = ttnn.from_torch(
+        data,
+        dtype=ttnn.bfloat16,
+        layout=ttnn.TILE_LAYOUT,
+        device=device,
+        memory_config=dram_memory_config,
+    )
+    b_tensor = ttnn.from_torch(
+        data,
+        dtype=ttnn.bfloat16,
+        layout=ttnn.TILE_LAYOUT,
+        device=device,
+        memory_config=dram_memory_config,
+    )
