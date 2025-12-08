77998a423ddb28fc17c4778ff7a8fed5c3c4e4a2
arichinstt <arichins@tenstorrent.com>
2025-12-08 10:59:31 -0800

    tilized slicing

--- examples/metal_singlecore_matmul.py
+++ examples/metal_singlecore_matmul.py
@@ -147,73 +147,76 @@ def test_singlecore_matmul_metal(M, K, N):
     ttnn.close_device(device)
 
 
-# from ttl import Program, make_circular_buffer_like, copy
+from ttl import Program, make_circular_buffer_like, copy
 
 
-# @ttl.kernel(grid=(1, 1))
-# def tt_lang_singlecore_matmul(a: ttnn.Tensor, b: ttnn.Tensor, out: ttnn.Tensor):
-#     assert a.shape[1] == b.shape[0], "Incompatible matrix shapes for multiplication."
-#     assert a.shape[0] == out.shape[0], "Output matrix has incorrect number of rows."
-#     blk_size = ttnn.TILE_SIZE
-#     M = a.shape[0]
-#     N = b.shape[1]
-#     K = a.shape[1]
-#     buffering_factor = 2
-#     a_cb = make_circular_buffer_like(
-#         a, shape=(blk_size, blk_size), buffer_factor=buffering_factor
-#     )
-#     b_cb = make_circular_buffer_like(
-#         b, shape=(blk_size, blk_size), buffer_factor=buffering_factor
-#     )
-#     out_cb = make_circular_buffer_like(
-#         out, shape=(blk_size, blk_size), buffer_factor=buffering_factor
-#     )
+@ttl.kernel(grid=(1, 1))
+def tt_lang_singlecore_matmul(a: ttnn.Tensor, b: ttnn.Tensor, out: ttnn.Tensor):
+    assert a.shape[1] == b.shape[0], "Incompatible matrix shapes for multiplication."
+    assert a.shape[0] == out.shape[0], "Output matrix has incorrect number of rows."
+    blk_size = ttnn.TILE_SIZE
+    M = a.shape[0]
+    N = b.shape[1]
+    K = a.shape[1]
+    Mt = M // blk_size
+    Kt = K // blk_size
+    Nt = N // blk_size
+    buffering_factor = 2
+    a_cb = make_circular_buffer_like(
+        a, shape=(blk_size, blk_size), buffer_factor=buffering_factor
+    )
+    b_cb = make_circular_buffer_like(
+        b, shape=(blk_size, blk_size), buffer_factor=buffering_factor
+    )
+    out_cb = make_circular_buffer_like(
+        out, shape=(blk_size, blk_size), buffer_factor=buffering_factor
+    )
 
-#     @ttl.compute()
-#     def mm_compute():
-#         for _ in range(M // blk_size):
-#             for _ in range(N // blk_size):
-#                 with out_cb.reserve() as out_blk:
-#                     for _ in range(K // blk_size):
-#                         with a_cb.wait() as a_blk, b_cb.wait() as b_blk:
-#                             out_blk += a_blk @ b_blk
+    @ttl.compute()
+    def mm_compute():
+        for _ in range(Mt):
+            for _ in range(Nt):
+                with out_cb.reserve() as out_blk:
+                    for _ in range(Kt):
+                        with a_cb.wait() as a_blk, b_cb.wait() as b_blk:
+                            out_blk += a_blk @ b_blk
 
-#     @ttl.datamovement()
-#     def mm_reader():
-#         for m in range(0, M, blk_size):
-#             for n in range(0, N, blk_size):
-#                 for k in range(0, K, blk_size):
-#                     with a_cb.reserve() as a_blk, b_cb.reserve() as b_blk:
-#                         a_wr = copy(a[m : (m + blk_size), k : (k + blk_size)], a_blk)
-#                         b_wr = copy(b[k : (k + blk_size), n : (n + blk_size)], b_blk)
-#                         a_wr.wait()
-#                         b_wr.wait()
+    @ttl.datamovement()
+    def mm_reader():
+        for m in range(Mt):
+            for n in range(Nt):
+                for k in range(Kt):
+                    with a_cb.reserve() as a_blk, b_cb.reserve() as b_blk:
+                        a_wr = copy(a[m : (m + 1), k : (k + 1)], a_blk)
+                        b_wr = copy(b[k : (k + 1), n : (n + 1)], b_blk)
+                        a_wr.wait()
+                        b_wr.wait()
 
-#     @ttl.datamovement()
-#     def mm_writer():
-#         for m in range(0, M, blk_size):
-#             for n in range(0, N, blk_size):
-#                 with out_cb.wait() as out_blk:
-#                     out_wr = copy(out_blk, out[m : (m + blk_size), n : (n + blk_size)])
-#                     out_wr.wait()
+    @ttl.datamovement()
+    def mm_writer():
+        for m in range(Mt):
+            for n in range(Nt):
+                with out_cb.wait() as out_blk:
+                    out_wr = copy(out_blk, out[m : (m + 1), n : (n + 1)])
+                    out_wr.wait()
 
-#     return Program(mm_compute, mm_reader, mm_writer)(a, b, out)
+    return Program(mm_compute, mm_reader, mm_writer)(a, b, out)
 
 
-# def test_singlecore_matmul_tt_lang():
-#     """Test singlecore matmul kernel."""
-#     device = ttnn.open_device(device_id=0)
-#     M, K, N = 256, 256, 256
-#     a = ttnn.rand((M, K), dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT)
-#     b = ttnn.rand((K, N), dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT)
-#     c = ttnn.empty((M, N), dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT)
+def test_singlecore_matmul_tt_lang():
+    """Test singlecore matmul kernel."""
+    device = ttnn.open_device(device_id=0)
+    M, K, N = 256, 256, 256
+    a = ttnn.rand((M, K), dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT)
+    b = ttnn.rand((K, N), dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT)
+    c = ttnn.empty((M, N), dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT)
 
-#     tt_lang_singlecore_matmul(a, b, c)
+    tt_lang_singlecore_matmul(a, b, c)
 
-#     golden = torch.matmul(
-#         ttnn.to_torch(a).to(torch.bfloat16), ttnn.to_torch(b).to(torch.bfloat16)
-#     )
-#     result = ttnn.to_torch(c).to(torch.bfloat16)
-#     assert_with_ulp(golden, result)
+    golden = torch.matmul(
+        ttnn.to_torch(a).to(torch.bfloat16), ttnn.to_torch(b).to(torch.bfloat16)
+    )
+    result = ttnn.to_torch(c).to(torch.bfloat16)
+    assert_with_ulp(golden, result)
 
-#     ttnn.close_device(device)
+    ttnn.close_device(device)
