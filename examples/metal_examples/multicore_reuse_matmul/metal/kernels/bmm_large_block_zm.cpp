753470e91de50498285cfbcfd9b5d08ee4c49ee8
arichinstt <arichins@tenstorrent.com>
2025-12-11 15:50:29 -0800

    multicore reuse

---
+++ examples/metal_examples/multicore_reuse_matmul/metal/kernels/bmm_large_block_zm.cpp
@@ -0,0 +1,122 @@
+// SPDX-FileCopyrightText: (c) 2025 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <cstdint>
+
+#include "compute_kernel_api/matmul.h"
+#include "compute_kernel_api/tile_move_copy.h"
+
+namespace NAMESPACE {
+void MAIN {
+  uint32_t K_block_size =
+      get_compile_time_arg_val(0); // inner block size in tiles
+  uint32_t a_num_subblocks =
+      get_compile_time_arg_val(1); // outer row block size (in inner row blocks)
+  uint32_t a_block_num_tiles = get_compile_time_arg_val(
+      2); // out_subblock_h*K_block_size*a_num_subblocks;
+  uint32_t a_subblock_num_tiles =
+      get_compile_time_arg_val(3); // out_subblock_h*K_block_size
+  uint32_t b_num_subblocks = get_compile_time_arg_val(
+      4); // outer column block size (in inner column blocks)
+  uint32_t b_block_num_tiles = get_compile_time_arg_val(
+      5); // out_subblock_w*K_block_size* b_num_subblocks;
+  uint32_t b_per_core_w =
+      get_compile_time_arg_val(6); // out_subblock_w*b_num_subblocks
+  uint32_t num_blocks =
+      get_compile_time_arg_val(7); // outer inner dim (in inner dim blocks)
+  uint32_t out_subblock_h =
+      get_compile_time_arg_val(8); // inner row block size in tiles
+  uint32_t out_subblock_w =
+      get_compile_time_arg_val(9); // inner column block size in tiles
+  uint32_t out_subblock_num_tiles =
+      get_compile_time_arg_val(10); // out_subblock_h * out_subblock_w;
+
+  mm_init(tt::CBIndex::c_0, tt::CBIndex::c_1, tt::CBIndex::c_16);
+
+  bool spill = num_blocks > 1;
+  bool enable_reload = false;
+  uint32_t out_num_tiles_to_wait = out_subblock_num_tiles;
+
+  for (uint32_t block = 0; block < num_blocks; block++) {
+    bool last_out = block == (num_blocks - 1);
+
+    cb_wait_front(tt::CBIndex::c_0, a_block_num_tiles);
+    cb_wait_front(tt::CBIndex::c_1, b_block_num_tiles);
+    // start of being handled by compiler
+    int a_index_subblock_offset = 0;
+    for (uint32_t a_subblock = 0; a_subblock < a_num_subblocks; a_subblock++) {
+      int b_index_subblock_offset = 0;
+      for (uint32_t b_subblock = 0; b_subblock < b_num_subblocks;
+           b_subblock++) {
+        acquire_dst();
+
+        if (enable_reload) {
+          copy_tile_to_dst_init_short(tt::CBIndex::c_24);
+          cb_wait_front(tt::CBIndex::c_24, out_subblock_num_tiles);
+          for (uint32_t i = 0; i < out_subblock_num_tiles; i++) {
+            copy_tile(tt::CBIndex::c_24, i, i);
+          }
+          cb_pop_front(tt::CBIndex::c_24, out_subblock_num_tiles);
+          mm_init_short(tt::CBIndex::c_0, tt::CBIndex::c_1);
+        }
+
+        // Compute output sub-block from a_subblock x b_subblock
+        int dst_index = 0;
+        int a_index_h_offset = 0;
+        for (uint32_t h = 0; h < out_subblock_h; h++) {
+          for (uint32_t w = 0; w < out_subblock_w; w++) {
+            int b_index_inner_dim_offset = 0;
+            for (uint32_t inner_dim = 0; inner_dim < K_block_size;
+                 inner_dim++) {
+              int a_index =
+                  a_index_subblock_offset + a_index_h_offset + inner_dim;
+              int b_index =
+                  b_index_subblock_offset + b_index_inner_dim_offset + w;
+              matmul_tiles(tt::CBIndex::c_0, tt::CBIndex::c_1, a_index, b_index,
+                           dst_index, false /* transpose */);
+              b_index_inner_dim_offset += b_per_core_w;
+            }
+            dst_index++;
+          }
+          a_index_h_offset += K_block_size;
+        }
+
+        if (last_out) {
+          // Pack out to output buffer
+          cb_reserve_back(tt::CBIndex::c_16, out_subblock_num_tiles);
+          for (uint32_t i = 0; i < out_subblock_num_tiles; i++) {
+            pack_tile(i, tt::CBIndex::c_16);
+          }
+          cb_push_back(tt::CBIndex::c_16, out_subblock_num_tiles);
+        } else {
+          // Wait for tiles in output buffer to be written out since interm
+          // and output share memory
+          if (block == 0) {
+            cb_reserve_back(tt::CBIndex::c_16, out_num_tiles_to_wait);
+            out_num_tiles_to_wait += out_subblock_num_tiles;
+          }
+          // Move partial result to interm buffer
+          cb_reserve_back(tt::CBIndex::c_24, out_subblock_num_tiles);
+          for (uint32_t i = 0; i < out_subblock_num_tiles; i++) {
+            pack_tile(i, tt::CBIndex::c_24);
+          }
+          cb_push_back(tt::CBIndex::c_24, out_subblock_num_tiles);
+        }
+
+        release_dst();
+
+        b_index_subblock_offset += out_subblock_w;
+      }
+      a_index_subblock_offset += a_subblock_num_tiles;
+    }
+
+    if (spill) {
+      enable_reload = true;
+    }
+    // end of compiler subblock generation
+    cb_pop_front(tt::CBIndex::c_0, a_block_num_tiles);
+    cb_pop_front(tt::CBIndex::c_1, b_block_num_tiles);
+  }
+}
+} // namespace NAMESPACE
---
+++ examples/metal_examples/multicore_reuse_matmul/ttlang/multicore_reuse_matmul.py
@@ -0,0 +1,110 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+import ttnn
+import pytest
+import torch
+
+from ttl import Program, make_circular_buffer_like, copy, core
+from metal_examples.utils import assert_with_ulp
+
+
+@ttl.kernel(grid=(13, 10))
+def tt_lang_multicore_matmul(a: ttnn.Tensor, b: ttnn.Tensor, out: ttnn.Tensor):
+    assert a.shape[1] == b.shape[0], "Incompatible matrix shapes for multiplication."
+    assert a.shape[0] == out.shape[0], "Output matrix has incorrect number of rows."
+    M = a.shape[0]
+    N = b.shape[1]
+    K = a.shape[1]
+    Mt = M // ttnn.TILE_SIZE
+    Kt = K // ttnn.TILE_SIZE
+    Nt = N // ttnn.TILE_SIZE
+
+    K_block_size = 2  # k dim block size
+
+    num_cores_x, num_cores_y = ttl.grid_size(dims=2)
+    # unused subblock sizes, to be determined by compiler, but using helper function to get better simultaneous comparisons
+    (per_core_M, per_core_N, _, _) = get_large_matmul_params(
+        Mt, Nt, num_cores_y, num_cores_x, K_block_size
+    )
+    assert per_core_M != 0, "get_large_matmul_params was not able to find a solution"
+    print(f"per_core_M: {per_core_M}, per_core_N: {per_core_N}")
+    assert Mt % per_core_M == 0, "per_core_M must divide Mt"
+    assert Nt % per_core_N == 0, "per_core_N must divide Nt"
+    assert Kt % K_block_size == 0, "K_block_size must divide Kt"
+    num_total_blocks = (Mt // per_core_M) * (Nt // per_core_N)
+    assert (
+        num_total_blocks <= num_cores_x * num_cores_y
+    ), "number of total blocks must be less than or equal to num cores"
+
+    buffering_factor = 2
+    a_cb = make_circular_buffer_like(
+        a, shape=(per_core_M, K_block_size), buffer_factor=buffering_factor
+    )
+    b_cb = make_circular_buffer_like(
+        b, shape=(K_block_size, per_core_N), buffer_factor=buffering_factor
+    )
+    # non buffered output, matching metal implementation
+    out_cb = make_circular_buffer_like(
+        out, shape=(per_core_M, per_core_N), buffer_factor=1
+    )
+
+
+    @ttl.compute()
+    def mm_compute():
+        with out_cb.reserve() as out_blk:  # per_core_M * per_core_N
+            for _ in range(Kt // K_block_size):
+                with a_cb.wait() as a_blk, b_cb.wait() as b_blk:  # a per_core_M x K_block_size, b K_block_size x per_core_N
+                    out_blk.store(out_blk + a_blk @ b_blk)
+                    
+
+    @ttl.datamovement()
+    def mm_reader():
+        core_x, core_y = ttl.core(dims=2)
+        out_row = per_core_M * core_y
+        out_col = per_core_N * core_x
+        for block in range(Kt // K_block_size):
+            k = block * K_block_size
+            with a_cb.reserve() as a_blk, b_cb.reserve() as b_blk:
+                a_wr = copy(
+                    a[out_row : (out_row + per_core_M), k : (k + K_block_size)], a_blk
+                )
+                b_wr = copy(
+                    b[k : (k + K_block_size), out_col : (out_col + per_core_N)], b_blk
+                )
+                a_wr.wait()
+                b_wr.wait()
+
+    # blocking only occurs on the k dim, so each core writes its entire output block at once
+    @ttl.datamovement()
+    def mm_writer():
+        core_x, core_y = ttl.core(dims=2)
+        out_row = per_core_M * core_y
+        out_col = per_core_N * core_x
+        with out_cb.wait() as out_blk:
+            out_wr = copy(
+                out_blk,
+                out[out_row : (out_row + per_core_M), out_col : (out_col + per_core_N)],
+            )
+            out_wr.wait()
+
+    return Program(mm_compute, mm_reader, mm_writer)(a, b, out)
+
+
+@pytest.mark.parametrize("M,K,N", [(256, 256, 256), (512, 512, 512)])
+def test_multicore_matmul_tt_lang(M, K, N):
+    """Test multicore matmul kernel."""
+    device = ttnn.open_device(device_id=0)
+    a = ttnn.rand((M, K), dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT)
+    b = ttnn.rand((K, N), dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT)
+    c = ttnn.empty((M, N), dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT)
+
+    tt_lang_multicore_matmul(a, b, c)
+
+    golden = torch.matmul(
+        ttnn.to_torch(a).to(torch.bfloat16), ttnn.to_torch(b).to(torch.bfloat16)
+    )
+    result = ttnn.to_torch(c).to(torch.bfloat16)
+    assert_with_ulp(golden, result)
+
+    ttnn.close_device(device)
