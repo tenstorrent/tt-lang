bd7a74e96f2dd9f945f06f802541decb18f91a1e
Boyana Norris <bnorris@tenstorrent.com>
2025-12-11 23:14:59 -0800

    cleanup, add todos

---
+++ include/ttlang/Dialect/TTL/IR/TTLOpsEnums.h
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef TTLANG_DIALECT_TTL_IR_TTLOPSENUMS_H
+#define TTLANG_DIALECT_TTL_IR_TTLOPSENUMS_H
+
+// Wrapper for TableGen-generated TTL enum declarations, will be used for future
+// enum definitions.
+#include "ttlang/Dialect/TTL/IR/TTLOpsEnums.h.inc"
+
+#endif // TTLANG_DIALECT_TTL_IR_TTLOPSENUMS_H
