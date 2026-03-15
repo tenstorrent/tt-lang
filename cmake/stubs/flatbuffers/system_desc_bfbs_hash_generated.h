e231ae37eabdc22cb0dd59018be5b58c9c70b037
Boyana Norris <bnorris@tenstorrent.com>
2026-03-14 21:24:58 -0700

    update tt-mlir, remove flatbuffers stubs

--- cmake/stubs/flatbuffers/system_desc_bfbs_hash_generated.h
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
-// SPDX-License-Identifier: Apache-2.0
-//
-// Stub: schema hash not available without flatbuffers.
-#pragma once
-#include <string_view>
-namespace tt::target::common {
-inline constexpr std::string_view system_desc_bfbs_schema_hash = "";
-} // namespace tt::target::common
