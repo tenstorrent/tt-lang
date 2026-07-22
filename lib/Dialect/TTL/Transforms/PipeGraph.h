c44a05f20f2550292e468f1597468f405a2ecfb8
Boyana Norris <bnorris@tenstorrent.com>
2026-07-22 09:13:14 -0700

    Test global ready-counter reuse across source cores

--- lib/Dialect/TTL/Transforms/PipeGraph.h
+++ lib/Dialect/TTL/Transforms/PipeGraph.h
@@ -369,8 +369,8 @@ private:
   LogicalResult rebuildEndpointGraph(const PipeTransferIndex &transferIndex,
                                      PipeGraphAnalysisState &state);
 
-  LogicalResult provePipeOnlyReceiverProducerStreams(
-      PipeGraphAnalysisState &analysisState);
+  LogicalResult
+  provePipeOnlyReceiverProducerStreams(PipeGraphAnalysisState &analysisState);
 
   llvm::MapVector<Operation *, ReceiverDFBInfo> receiverDFBByPost;
   SmallVector<PipeTransferNode, 0> pipeTransferNodes;
