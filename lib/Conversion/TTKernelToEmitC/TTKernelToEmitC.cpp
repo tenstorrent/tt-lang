37a238f2cf308d362a2a7f40cd14c74437a2b1ea
Boyana Norris <bnorris@tenstorrent.com>
2026-08-06 19:03:36 -0700

    [ttl] Preserve gamma broadcast semantics in RMSNorm fusion

--- lib/Conversion/TTKernelToEmitC/TTKernelToEmitC.cpp
+++ lib/Conversion/TTKernelToEmitC/TTKernelToEmitC.cpp
@@ -2672,8 +2672,6 @@ public:
                                std::to_string(op.getNumTiles())),
         emitc::OpaqueAttr::get(op.getContext(),
                                op.getHasGamma() ? "true" : "false"),
-        emitc::OpaqueAttr::get(op.getContext(),
-                               op.getRepeatGamma() ? "true" : "false"),
         datatypeToDataformatEnumNameOpaqueAttr(rewriter, op.getDtype())};
     SmallVector<Value, 5> operands = {
         adaptor.getInputCb(), adaptor.getGammaCb(), adaptor.getOutputCb(),
