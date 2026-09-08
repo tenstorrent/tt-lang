75007e69fa2243203be84e858268fbed15f1eb4e
Boyana Norris <bnorris@tenstorrent.com>
2026-09-07 19:32:34 -0700

    Format compiler L1 fabric receiver metadata

--- lib/Dialect/TTL/Transforms/PipeGraph.cpp
+++ lib/Dialect/TTL/Transforms/PipeGraph.cpp
@@ -2361,14 +2361,9 @@ PipeGraph::addPipeReceiver(Operation *op, PipeTransferCreateOp transferCreateOp,
     return failure();
   }
   ReceiverDFBInfo receiverInfo{
-      *maybeDFBIndex,
-      *maybeDFBId,
-      dfbType,
-      hasStaticTileOffset,
-      staticTileOffset,
-      *slotSpanBlocks,
-      dfbType.getBlockCount(),
-      op->getLoc()};
+      *maybeDFBIndex,          *maybeDFBId,      dfbType,
+      hasStaticTileOffset,     staticTileOffset, *slotSpanBlocks,
+      dfbType.getBlockCount(), op->getLoc()};
   bool inserted = receiverDFBByPost.insert({op, receiverInfo}).second;
   assert(inserted && "receiver post visited more than once");
   return success();
