010f17d4c486f12ef8cb336776e689b5ee69335e
Boyana Norris <bnorris@tenstorrent.com>
2026-09-13 03:42:05 +0000

    [ttkernel] Validate Boolean negation in compiler L1

--- lib/Conversion/TTKernelToEmitC/TTKernelToEmitC.cpp
+++ lib/Conversion/TTKernelToEmitC/TTKernelToEmitC.cpp
@@ -3594,6 +3594,9 @@ public:
         return;
       }
       WalkResult validation = module.walk([&](Operation *operation) {
+        if (isa<emitc::LogicalNotOp>(operation)) {
+          return WalkResult::advance();
+        }
         if (operation->getName().getDialectNamespace() == "emitc") {
           operation->emitOpError(
               "compiler-l1 cannot validate pre-lowered C++ effects");
